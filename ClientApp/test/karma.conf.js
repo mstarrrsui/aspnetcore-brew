be2f2c66f1deabb119a1f648e893ad82814df373
Mike Starr <mstarr@rsui.com>
2017-05-24 15:17:46 -0400

    sourcemaps showing with karma

--- ClientApp/test/karma.conf.js
+++ ClientApp/test/karma.conf.js
@@ -1,7 +1,11 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
+const path = require('path');
+const clientBundleOutputDir = '../../wwwroot/dist';
+const webpack = require('webpack');
+const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
 
-module.exports = function (config) {
+module.exports = function(config) {
     config.set({
         basePath: '.',
         frameworks: ['jasmine'],
@@ -10,17 +14,49 @@ module.exports = function (config) {
             './boot-tests.ts'
         ],
         preprocessors: {
-            './boot-tests.ts': ['webpack']
+            './boot-tests.ts': ['webpack', 'sourcemap']
         },
-        reporters: ['progress'],
+        reporters: ['mocha', 'kjhtml'],
         port: 9876,
         colors: true,
-        logLevel: config.LOG_INFO,
+        logLevel: config.LOG_DEBUG,
         autoWatch: true,
         browsers: ['Chrome'],
-        mime: { 'application/javascript': ['ts','tsx'] },
+        mime: { 'text/x-typescript': ['ts', 'tsx'] },
         singleRun: false,
-        webpack: require('../../webpack.config.js')().filter(config => config.target !== 'node'), // Test against client bundle, because tests run in a browser
+
+
+
+        webpack: {
+            stats: { modules: false },
+            context: path.resolve(__dirname, "../.."),
+            resolve: { extensions: ['.js', '.ts'] },
+            output: {
+                filename: '[name].js',
+                publicPath: '/dist/', // Webpack dev middleware, if enabled, handles requests for this URL prefix
+                path: path.join(__dirname, './wwwroot/dist')
+            },
+            module: {
+                rules: [
+                    { test: /\.ts$/, include: /ClientApp/, use: ['awesome-typescript-loader?silent=true', 'angular2-template-loader'] },
+                    { test: /\.html$/, use: 'html-loader?minimize=false' },
+                    { test: /\.css$/, use: ['to-string-loader', 'css-loader'] },
+                    { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+                ]
+            },
+            entry: { 'main-client': './ClientApp/boot-client.ts' },
+            plugins: [new CheckerPlugin(),
+                new webpack.DllReferencePlugin({
+                    context: __dirname,
+                    manifest: require('../../wwwroot/dist/vendor-manifest.json')
+                }),
+                new webpack.SourceMapDevToolPlugin({
+                    test: /\.(ts|js)($|\?)/i
+                })
+            ],
+            devtool: 'inline-source-map'
+        },
+
         webpackMiddleware: { stats: 'errors-only' }
     });
 };
